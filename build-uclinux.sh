a27deb961ef16f3300b607c5d74584f624b1b853
robacklin <rob.acklin@gmail.com>
2012-12-03 18:58:35 -0500

    updated build script for exports

--- build-uclinux.sh
+++ build-uclinux.sh
@@ -16,10 +16,11 @@ if [ ! -d "arm-2009q1" ] ; then
 	wget http://www.ackatech.net/downloads/arm-2009q1.tar.bz2
 	fail_not_exist arm-2009q1.tar.bz2
 	tar xvjf arm-2009q1.tar.bz2
-
-	export ARCH=arm
-	export CROSS_COMPILE=`pwd`/arm-2009q1/bin/arm-none-linux-gnueabi-
 fi
+
+export ARCH=arm
+export CROSS_COMPILE=`pwd`/arm-2009q1/bin/arm-none-linux-gnueabi-
+
 
 #cd uClinux-dist
 
