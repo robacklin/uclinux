89311ff47a6fef5763df4f88a847f1b60ba50a94
robacklin <rob.acklin@gmail.com>
2012-12-08 18:17:09 -0500

    update build scripts

--- build-uclinux.sh
+++ build-uclinux.sh
@@ -1,31 +1,12 @@
 #!bin/sh
 
-fail_not_exist() {
-	if [ ! -r "$1" ] ; then
-		echo "not found $1"
-		exit 1
-	fi
-}
-
-#if [ ! -d "uClinux-dist" ] ; then
-#	wget http://www.uclinux.org/pub/uClinux/dist/uClinux-dist-20120620.tar.bz2
-#	tar jxvf uClinux-dist-20120620.tar.bz2
-#fi
-
-if [ ! -d "arm-2009q1" ] ; then
-	wget http://www.ackatech.net/downloads/arm-2009q1.tar.bz2
-	fail_not_exist arm-2009q1.tar.bz2
-	tar xvjf arm-2009q1.tar.bz2
+if [ -d moonshine ] ; then
+	cd moonshine ; git pull
+	cd ..
+else
+	git clone git://github.com/robacklin/moonshine.git
 fi
 
-export ARCH=arm
-export CROSS_COMPILE=`pwd`/arm-2009q1/bin/arm-none-linux-gnueabi-
 
+sh ./moonshine/scripts/build-uclinux.sh
 
-#cd uClinux-dist
-
-cd linux-3.x
-make mrproper
-#make menuconfig
-#make O=/home/racklin/Projects/builds/uclinux/linux dep
-#make 
