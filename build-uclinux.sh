69883fd6099b5ad29c11bd3c1629d71701dc2310
robacklin <rob.acklin@gmail.com>
2012-12-08 22:14:00 -0500

    removed uClibc and moved it to uclinux-uclibc

--- build-uclinux.sh
+++ build-uclinux.sh
@@ -12,7 +12,7 @@ ms_git_pull() {
 }
 
 ms_find_replace_gnueabi() {
-	grep -R "arm-linux-" * > /tmp/arm-linux-log.txt
+	grep -R "arm-linux-" * | egrep -v "Documentation"  > /tmp/arm-linux-log.txt
 	cat /tmp/arm-linux-log.txt | gawk -F':' '{print $1}' | sort -u > /tmp/arm-linux-gnueabi-log.txt
 	for x in `cat /tmp/arm-linux-gnueabi-log.txt` ; do sed -i 's,arm-linux-,arm-linux-gnueabi-,g' "$x" ; done
 }
@@ -20,10 +20,10 @@ ms_find_replace_gnueabi() {
 #ms_git_pull git://github.com/robacklin/uclinux-freeswan.git freeswan
 #ms_git_pull git://github.com/robacklin/uclinux-glibc.git glibc
 ms_git_pull git://github.com/robacklin/uclinux-linux-2.0.x.git linux-2.0.x
-#ms_git_pull git://github.com/robacklin/uclinux-linux-2.4.x.git linux-2.4.x
-#ms_git_pull git://github.com/robacklin/uclinux-linux-3.x.git linux-3.x
+ms_git_pull git://github.com/robacklin/uclinux-linux-2.4.x.git linux-2.4.x
+ms_git_pull git://github.com/robacklin/uclinux-linux-3.x.git linux-3.x
 #ms_git_pull git://github.com/robacklin/uclinux-openswan.git openswan
-#ms_git_pull git://github.com/robacklin/uclinux-uclib.git uClib
+ms_git_pull git://github.com/robacklin/uclinux-uclibc.git uClib
 ms_git_pull git://github.com/robacklin/uclinux-users.git user
 ms_git_pull git://github.com/robacklin/moonshine.git moonshine
 
